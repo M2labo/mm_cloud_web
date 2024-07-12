975b356b923d7474519c47fec42a8eb3d629c5fb
KoheiNatsume <mecchi624@gmail.com>
2024-07-12 19:59:55 +0900

    地図にずれを表示

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,27 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Polyline, useMap, Polygon, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Polyline, useMap, Polygon, Popup, Marker, useMapEvent } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import { getUrl } from "@aws-amplify/storage";
-import { LatLngTuple } from "leaflet";
+import { LatLngTuple, LatLngExpression } from "leaflet";
 
-
-// mmId、date、logIdを使用してROUTE.csvファイルのURLを取得し、その内容をテキストとして返す関数
-async function getMavlink(mmId: string | undefined, date: string | undefined, logId: string | undefined): Promise<string> {
+async function getRoute(mmId: string | undefined, date: string | undefined, logId: string | undefined): Promise<string> {
     const urlResponse = await getUrl({ key: `${mmId}/${date?.slice(0, 4)}/${date?.slice(4, 8)}/${logId}/ROUTE.csv` });
     const response = await fetch(urlResponse.url.href);
     return response.text();
 }
 
-// APIエンドポイントからポリゴンデータを取得する関数
 async function fetchPolygons(): Promise<any> {
     const response = await fetch('https://lsdlueq272y5yboojqgls6dcsi0ejsla.lambda-url.ap-northeast-1.on.aws/all_field');
     const data = await response.json();
     return data;
 }
 
-// APIエンドポイントからルートデータを取得する関数
 async function fetchRoute(mmId: string | undefined, date: string | undefined, logId: string | undefined): Promise<any> {
-    const fiter_dict = {mm: mmId, date: date, time: logId};
+    const fiter_dict = { mm: mmId, date: date, time: logId };
     const queryParams = new URLSearchParams({ filter: JSON.stringify(fiter_dict) });
     console.log(`https://lsdlueq272y5yboojqgls6dcsi0ejsla.lambda-url.ap-northeast-1.on.aws/log?${queryParams}`);
     const response = await fetch(`https://lsdlueq272y5yboojqgls6dcsi0ejsla.lambda-url.ap-northeast-1.on.aws/log?${queryParams}`);
@@ -29,7 +25,6 @@ async function fetchRoute(mmId: string | undefined, date: string | undefined, lo
     return data;
 }
 
-// マップの中心を変更するコンポーネント
 interface ChangeMapCenterProps {
     position: LatLngTuple;
 }
@@ -40,19 +35,58 @@ function ChangeMapCenter({ position }: ChangeMapCenterProps) {
     return null;
 }
 
-// Mapコンポーネント
-export const Map: React.FC<{ mmId?: string | undefined; date?: string | undefined; logId?: string | undefined; size?:string; zoom?:number}> = ({ mmId, date, logId, size, zoom }) => {
+const calculateAverage = (numbers: number[]): number => {
+    const validNumbers = numbers.filter(num => !isNaN(num));
+    const total = validNumbers.reduce((acc, num) => acc + num, 0);
+    return validNumbers.length ? total / validNumbers.length : 0;
+}
+
+const HoverMarker: React.FC<{ position: LatLngTuple; difference: number; averageDifference: number }> = ({ position, difference, averageDifference }) => {
+    return (
+
+        <Popup position={position}>
+            ずれ : {(difference*100).toPrecision(3)} cm<br />
+            平均 : {(averageDifference * 100).toPrecision(3)} cm
+        </Popup>
+
+    );
+}
+
+const PolylineWithHover: React.FC<{ positions: LatLngTuple[][], differences: number[] }> = ({ positions, differences }) => {
+    const [hoverData, setHoverData] = useState<{ position: LatLngTuple; difference: number } | null>(null);
+    const averageDifference = calculateAverage(differences);
+
+    useMapEvent('mousemove', (e) => {
+        const { lat, lng } = e.latlng;
+        positions.forEach((line, lineIndex) => {
+            line.forEach((point, pointIndex) => {
+                if (Math.abs(point[0] - lat) < 0.000001 && Math.abs(point[1] - lng) < 0.000001) {
+                    setHoverData({ position: [point[0], point[1]], difference: differences[pointIndex] });
+                }
+            });
+        });
+    });
+
+    return (
+        <>
+            <Polyline pathOptions={{ color: 'lime' }} positions={positions} />
+            {hoverData && <HoverMarker position={hoverData.position} difference={hoverData.difference} averageDifference={averageDifference} />}
+        </>
+    );
+}
+
+export const Map: React.FC<{ mmId?: string | undefined; date?: string | undefined; logId?: string | undefined; size?: string; zoom?: number; center?: LatLngTuple; }> = ({ mmId, date, logId, size, zoom, center }) => {
     const [dataFrame, setDataFrame] = useState<string[][]>([]);
     const [multiPolyline, setMultiPolyline] = useState<LatLngTuple[][]>([[]]);
     const [routePolyline, setRoutePolyline] = useState<LatLngTuple[][]>([[]]);
-    const [position, setPosition] = useState<LatLngTuple>([36.252261, 137.866767]);
+    const [position, setPosition] = useState<LatLngTuple>(center ? center : [36.252261, 137.866767]);
     const [polygons, setPolygons] = useState<{ id: number; name: string; customer: string; customer_id: number; polygon: LatLngTuple[] }[]>([]);
+    const [differences, setDifferences] = useState<number[]>([]);
 
-    // mmId、date、logIdが変更されたときにROUTE.csvファイルを取得し、dataFrameにセットする
     useEffect(() => {
         let isSubscribed = true;
         if (!mmId || !date || !logId) return;
-        getMavlink(mmId, date, logId)
+        getRoute(mmId, date, logId)
             .then(data => {
                 if (isSubscribed) {
                     const dataRows: string[][] = [];
@@ -68,27 +102,29 @@ export const Map: React.FC<{ mmId?: string | undefined; date?: string | undefine
         return () => { isSubscribed = false; };
     }, [mmId, date, logId]);
 
-    // dataFrameが変更されたときに、multiPolylineとpositionを更新する
     useEffect(() => {
         if (!mmId || !date || !logId) return;
         const newMultiPolyline: LatLngTuple[][] = [[]];
+        const newDifferences: number[] = [];
         let newPosition: LatLngTuple = [0, 0];
 
         dataFrame.forEach(row => {
             const lat = parseFloat(row[0]) / 10000000;
             const lng = parseFloat(row[1]) / 10000000;
+            const difference = parseFloat(row[4]);
 
             if (!isNaN(lat) && !isNaN(lng)) {
                 newMultiPolyline[0].push([lat, lng]);
+                newDifferences.push(difference);
                 newPosition = [lat, lng];
             }
         });
 
         setMultiPolyline(newMultiPolyline);
+        setDifferences(newDifferences);
         setPosition(newPosition);
     }, [dataFrame]);
 
-    // コンポーネントがマウントされたときにポリゴンデータを取得し、polygonsにセットする
     useEffect(() => {
         let isSubscribed = true;
         fetchPolygons()
@@ -109,7 +145,6 @@ export const Map: React.FC<{ mmId?: string | undefined; date?: string | undefine
         return () => { isSubscribed = false; };
     }, []);
 
-    // コンポーネントがマウントされたときにルートを取得し、routePolylineにセットする
     useEffect(() => {
         let isSubscribed = true;
         console.log(mmId, date, logId);
@@ -129,15 +164,13 @@ export const Map: React.FC<{ mmId?: string | undefined; date?: string | undefine
 
     return (
         <div>
-            <MapContainer center={position} zoom={zoom?zoom:18} maxZoom={25} style={{ height: size?size:"50vh" }}>
+            <MapContainer center={position} zoom={zoom ? zoom : 18} maxZoom={25} style={{ height: size ? size : "50vh" }}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {/* CSVファイルから取得した経路データを表示 */}
                 <Polyline pathOptions={{ color: 'grey' }} positions={routePolyline} />
-                <Polyline pathOptions={{ color: 'lime' }} positions={multiPolyline} />                
-                {/* APIから取得したポリゴンデータを表示 */}
+                <PolylineWithHover positions={multiPolyline} differences={differences} />
                 {polygons.map(polygon => (
                     <Polygon key={polygon.id} pathOptions={{ color: 'red' }} positions={polygon.polygon}>
                         <Popup>
@@ -145,7 +178,6 @@ export const Map: React.FC<{ mmId?: string | undefined; date?: string | undefine
                         </Popup>
                     </Polygon>
                 ))}
-                {/* マップの中心を変更 */}
                 <ChangeMapCenter position={position} />
             </MapContainer>
         </div>
