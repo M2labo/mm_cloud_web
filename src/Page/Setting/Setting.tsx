c759ae7fd47f254ec0f071af9ccc841fb8f56bb0
masahirok5 <14masahirok.pec.vine20@gmail.com>
2025-04-28 14:13:37 +0900

    設定ページの管理者のみ見れる機能の追加

--- src/Page/Setting/Setting.tsx
+++ src/Page/Setting/Setting.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import {useUser} from "../../UserContext";
 
 export const Setting = () => {
+  const {user} = useUser()
+
   return (
     <>
       <ul>
-        <li><a href="#setting/profile">プロフィールの設定</a></li>
-        <li><a href="#setting/group">組織の設定</a></li>
-        <li><a href="#setting/mover">Moverの設定</a></li>
-        <li><a href="#setting/field">ほ場の設定</a></li>
-        <button onClick={() => alert('アプリバージョン: 1.0.0')}>バージョン情報</button>
-        <li><a href="#setting/terms_of_service">利用規約</a></li>
-        <li><a href="#setting/privacy_policy">プライバシーポリシー</a></li>
+        <li className="flex items-center justify-center py-2"><a href="#setting/profile">プロフィールの設定</a></li>
+        {user?.admin && <>
+          <li className="flex items-center justify-center py-2"><a href="#setting/group">組織の設定</a></li>
+          <li className="flex items-center justify-center py-2"><a href="#setting/mover">Moverの設定</a></li>
+          <li className="flex items-center justify-center py-2"><a href="#setting/field">ほ場の設定</a></li>
+        </>}
+        <li className="flex items-center justify-center py-2">
+          <button onClick={() => alert('アプリバージョン: 1.0.0')}>バージョン情報</button>
+        </li>
+        <li className="flex items-center justify-center py-2"><a href="#setting/terms_of_service">利用規約</a></li>
+        <li className="flex items-center justify-center py-2"><a href="#setting/privacy_policy">プライバシーポリシー</a>
+        </li>
       </ul>
     </>
   )
